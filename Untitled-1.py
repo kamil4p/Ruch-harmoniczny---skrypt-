5964e61f1906754c5c71bf9f5b86cc6d716870e0
kamil4p <kamila.pienczuk@gmail.com>
2025-05-18 21:25:07 +0200

    18.05

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,10 +1,42 @@
-print ("github ogarniety")
+#biblioteki
+import numpy as np
+import matplotlib as mat
 
-value_x = float(input("wpisz liczbe: "))
 
-print (value_x*2)
+def funkcjawar(prompt):
+    while True:
+        value = input(prompt)
+        try: 
+            value = float(value)
+            if value <= 0:
+                print ("Błąd - wartości muszą być większe od zera. Spróbuj ponownie.")
+            else:
+                return value
+        except ValueError:
+            print("Błąd. Podaj poprawne liczby.")
+    
+        
+#Dane
+omega = funkcjawar("Wprowadź prędkość kątową (rad/s^2): ")
+A = funkcjawar("Wprowadź amplitudę A (m):" )
+k = funkcjawar("Wprowadź współczynnik sprężystości k (N/m): ")
 
-print ("zmiana")
 
-print ("jest moc")
+
+# Obliczenia
+f = omega / (2 * np.pi)
+T = 1 / f
+E = 0.5 * k * A**2 
+
+#Wyniki
+print("-----Wyniki-----")
+print(f"Częstotliwość: {f:.3f} Hz")
+print(f"Okes: {T:.3f} s")
+print(f"Okres: {E:.3f} J")
+
+
+      
+
+
+
 
